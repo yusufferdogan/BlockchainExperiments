360007b370aeb84531e33a81081ae7f33c4e0b52
yusufferdogan <yusuffx03@gmail.com>
2022-03-18 23:10:16 +0300

    added component feature

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+fetch("./components/navbar/navbar.html")
+    .then(stream => stream.text())
+    .then(text => define(text));
+
+function define(html) {
+    class MyNavBar extends HTMLElement {
+        constructor() {
+            super();
+            var shadow = this.attachShadow({
+                mode: 'open'
+            });
+            shadow.innerHTML = html;
+        }
+    }
+    
+    customElements.define('my-nav-bar', MyNavBar);
+}
