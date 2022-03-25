2dc57d383dc07d0f256c49eb8030f4613ec70f6f
yusufferdogan <yusuffx03@gmail.com>
2022-03-25 15:31:05 +0300

    crud initial commit

---
+++ src/Smart_Contracts/crud.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: UNLICENSED
+contract crud {
+    address public owner;
+    struct person {
+        string name;
+    }
+
+    mapping (address => person) public persons;
+
+    
+
+
+
+}
