02f5cc34019c53a908b2899a813cb796b10c262c
yusufferdogan <yusuffx03@gmail.com>
2022-03-25 17:35:55 +0300

    added map and list

--- src/Smart_Contracts/crud.sol
+++ src/Smart_Contracts/crud.sol
@@ -1,14 +1,34 @@
 //SPDX-License-Identifier: UNLICENSED
+pragma solidity^0.8.13;
 contract crud {
     address public owner;
-    struct person {
+    struct Person {
         string name;
     }
 
-    mapping (address => person) public persons;
+    struct PersonWithId {
+        uint id;
+        string name;
+    }
 
+    uint nextId = 0;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    mapping (address => Person) public persons;
+
+    PersonWithId[] public personList;
+
+    function addNameToList(string memory name) external {
+        personList.push(PersonWithId(nextId,name));
+        nextId ++;
+    }
+
+    function addNameToMap(string memory name) external {
+        persons[msg.sender].name = name;
+
+    }
     
-
-
-
 }
