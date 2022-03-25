bd5e120c2cf5a98b345479796069f59655fd6eeb
yusufferdogan <yusuffx03@gmail.com>
2022-03-25 12:58:34 +0300

    learned about msg  object

--- src/Smart_Contracts/set_value.sol
+++ src/Smart_Contracts/set_value.sol
@@ -4,12 +4,21 @@ pragma solidity^0.8.13;
 contract SetValue{
     address public owner;
     uint256 public value;
+    bytes public data;
+    uint public gas;
+    bytes4 public sig;
+    uint public MsgValue;
 
-    constructor() { // constructor nasıl run edilir 
-        owner = msg.sender;
+    constructor(uint256 initialValue) { // constructor nasıl run edilir 
+        value = initialValue;
+        owner = msg.sender; //sender of the message (current call)
+        //sig = msg.sig; // must be payable 
+        //MsgValue = msg.value; // number of wei sent with the message
     }
 
     function setValue(uint256 _value) external{
+        data = msg.data;
+        gas = gasleft(); //remaining gas 
         value = _value;
     }
 
