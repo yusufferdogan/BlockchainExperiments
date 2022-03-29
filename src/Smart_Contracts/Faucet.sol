c8e91f6303a6449cd4df4a85b2953144ca260d13
yusufferdogan <yusuffx03@gmail.com>
2022-03-29 19:54:16 +0300

    Faucet contract upgraded

--- src/Smart_Contracts/Faucet.sol
+++ src/Smart_Contracts/Faucet.sol
@@ -1,9 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.7;
 
-contract FeeCollector {
+// import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+abstract contract ReentrancyGuard {
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+    modifier nonReentrant() {
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+        _status = _ENTERED;
+        _;
+        _status = _NOT_ENTERED;
+    }
+}
+
+contract Faucet is ReentrancyGuard{
+
     address public owner;
     uint256 public balance;
+
+    mapping (address => uint) public withdraws; 
 
     constructor() {
         owner = msg.sender;
@@ -13,10 +34,13 @@ contract FeeCollector {
         balance += msg.value;
     }
 
-    function withdraw(uint amount , address payable destAddress) public{
-        //require(msg.sender == owner , "Only owner can be withdraw");
+    // use external to save gas when function never called by contract itself
+    function withdraw(uint amount) external nonReentrant{
+        address payable destAddress = payable(msg.sender);
+        require(destAddress != address(0),"Address can't be null");
         require(amount <= balance,"insufficient funds");
+        balance = balance - amount;
+        withdraws[msg.sender] += amount; 
         destAddress.transfer(amount);
-        balance -= amount;
     }
 }
