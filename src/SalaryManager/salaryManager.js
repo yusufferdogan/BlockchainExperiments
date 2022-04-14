9e9fc4bf503107501d2a191cb55dcc32e4c0fe49
yusufferdogan <yusuffx03@gmail.com>
2022-04-14 12:54:42 +0300

    salary manager called sc methods

--- src/SalaryManager/salaryManager.js
+++ src/SalaryManager/salaryManager.js
@@ -9,42 +9,78 @@ if (typeof web3 !== 'undefined') {
     window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
-const addresses = ["0xc7115557e58EeD59471c5BE7748eF284938B4a5d","0x444DE5308e5AfB65CC6000cAaaCFc9800a1Df493"]
-const amounts = [Web3Lib.etherToWei(20) ,Web3Lib.etherToWei(20)]
+//*****************************************************************************
+const addresses = ["0xc7115557e58EeD59471c5BE7748eF284938B4a5d","0x07147406D726df8C84576d07011a6B14894dD6bD"]
+const amounts = [300 , 200]
+const amountsAsWei = amounts.map(amount => Web3Lib.etherToWei(amount))
 const total = amounts.reduce((a, b) => a + b, 0)
-const button = document.getElementById("submit_button");
-
-
+//*****************************************************************************
 const jsonInterface = JSON.parse(JSON.stringify(SalaryManagerAbi.myAbi()));
-const contractAddress = "0x7f6dbc21480b85fa6f4b7ba1db5de464534e8d7b";
+const contractAddress = "0xB390a772D89078e529ec3E8510f39455D4d5af97";
 const contract = new web3.eth.Contract(jsonInterface, contractAddress);
-
+//*****************************************************************************
 const usdtJsonInterface = JSON.parse(JSON.stringify(UsdtAbi.myAbi()));
-const usdtContractAddress = "0x77c24f0Af71257C0ee26e0E0a108F940D1698d53";
+const usdtContractAddress = "0xD9BA894E0097f8cC2BBc9D24D308b98e36dc6D02";
 const usdtContract = new web3.eth.Contract(usdtJsonInterface, usdtContractAddress);
 
 
+const button = document.getElementById("submit_button");
 button.addEventListener("click", async function (e) {
     const userAccount = await Web3Lib.getAccount();
-    await usdtContract.methods.approve(userAccount,total).send(
+    await allowance(userAccount);
+    await approve(userAccount);
+    await pay(userAccount);
+    await allowance(userAccount);
+})
+
+async function pay(userAccount){
+    await contract.methods.payUsdt(usdtContractAddress,addresses,amountsAsWei).send(
         { from: userAccount }, async function (err, result) {
             if (err) {
                 console.log(err);
             }
             else {
-                // balanceOfTheAccountField.innerHTML = Web3Lib.divideByTenToSix(result)
-            }
-        }
-    );
-    await contract.methods.pay(usdtContractAddress,addresses,amounts).send(
-        { from: userAccount }, async function (err, result) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                // balanceOfTheAccountField.innerHTML = Web3Lib.divideByTenToSix(result)
+                console.log(result);
             }
         }
     ); 
+}
 
-})
+async function allowance(userAccount){
+    await usdtContract.methods.allowance(userAccount,contractAddress).call(
+        { from: userAccount }, async function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log("allowance is :" + Web3Lib.weiToEther(result))
+            }
+        }
+    );
+}
+
+async function getUsdtBalance(userAccount){
+    await usdtContract.methods.balanceOf(userAccount).call(
+        { from: userAccount }, async function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log("balanceOf: " + Web3Lib.weiToEther(result))
+            }
+        }
+    );
+}
+
+async function approve(userAccount){
+    await usdtContract.methods.approve(contractAddress,Web3Lib.etherToWei(total)).send(
+        { from: userAccount }, async function (err, result) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(result)
+            }
+        }
+    );
+}
